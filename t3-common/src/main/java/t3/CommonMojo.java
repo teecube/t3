77d33d8783338982381d008906609a612fa56732
Mathieu Debove <mad@teecu.be>
2017-10-11 10:46:19 +0200

    Add compress Zip method

--- t3-common/src/main/java/t3/CommonMojo.java
+++ t3-common/src/main/java/t3/CommonMojo.java
@@ -24,8 +24,10 @@ import static org.twdata.maven.mojoexecutor.MojoExecutor.groupId;
 import static org.twdata.maven.mojoexecutor.MojoExecutor.plugin;
 import static org.twdata.maven.mojoexecutor.MojoExecutor.version;
 
+import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -44,6 +46,10 @@ import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.compress.archivers.ArchiveException;
+import org.apache.commons.compress.archivers.ArchiveOutputStream;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecuteResultHandler;
 import org.apache.commons.exec.DefaultExecutor;
@@ -1278,4 +1284,49 @@ public class CommonMojo extends AbstractMojo {
 			public void write(int b) {}
 		}));
 	}
+
+    /**
+     * Add all files from the source directory to the destination zip file
+     *
+     * @param source      the directory with files to add
+     * @param destination the zip file that should contain the files
+     * @throws IOException      if the io fails
+     * @throws ArchiveException if creating or adding to the archive fails
+     */
+    protected void addFilesToZip(File source, File destination) throws IOException, ArchiveException {
+        OutputStream archiveStream = new FileOutputStream(destination);
+        ArchiveOutputStream archive = new ArchiveStreamFactory().createArchiveOutputStream(ArchiveStreamFactory.ZIP, archiveStream);
+
+        Collection<File> fileList = FileUtils.listFiles(source, null, true);
+
+        for (File file : fileList) {
+            String entryName = getEntryName(source, file);
+            ZipArchiveEntry entry = new ZipArchiveEntry(entryName);
+            archive.putArchiveEntry(entry);
+
+            BufferedInputStream input = new BufferedInputStream(new FileInputStream(file));
+
+            IOUtils.copy(input, archive);
+            input.close();
+            archive.closeArchiveEntry();
+        }
+
+        archive.finish();
+        archiveStream.close();
+    }
+
+    /**
+     * Remove the leading part of each entry that contains the source directory name
+     *
+     * @param source the directory where the file entry is found
+     * @param file   the file that is about to be added
+     * @return the name of an archive entry
+     * @throws IOException if the io fails
+     */
+    private String getEntryName(File source, File file) throws IOException {
+        int index = source.getAbsolutePath().length() + 1;
+        String path = file.getCanonicalPath();
+
+        return path.substring(index);
+    }
 }
